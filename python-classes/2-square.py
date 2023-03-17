3e2d275625ff118209968153b02c2319988b11d0
cynthianekesa <c.nekesa@alustudent.com>
2023-03-16 22:44:04 -0700

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""Square class definition"""
+""" defines a square """
 
 
 class Square:
-    """Represents a square
-    Attributes:
-        __size (int): size of a side of the square
-    """
-    def __init__(self, size):
-        """Initializes a square
-        Args:
-            size (int): size of a side of the square
-        Returns: None
+    """ square with private instance attribute size """
+
+    def __init__(self, size=0):
         """
-        self.__size = size
+        Args:
+            size: size of square
+        """
+
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
